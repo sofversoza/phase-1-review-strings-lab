d4119cbcf7cd2c1050a4182ca2cb1bf43fd8250f
sofiaversoza <sofversoza@gmail.com>
2021-11-22 15:42:12 -0700

    commit tree 263e1b208ec66bc820833c1b05d5a4a19741b434

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+const currentUser = 'Sofia';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
